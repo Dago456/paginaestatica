c5964e5357ae0beee811c7f765ce969dd65e17a3
DAGO <christiandavidgomez466@gmail.com>
2023-01-29 19:38:59 -0500

    se sgregoel proyecto a git

---
+++ pages/about.js
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import { Inter } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+
+export default function aboutpage() {
+  return (
+    <div className={styles.container}>
+
+      <head>
+        <title>Home - practica</title>
+        <meta name="description" content="about Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Ir a <a href="/">home</a>
+        </h1>
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
+        </p>)
+      </main>
+    </div>
+  )
+}
+console.show("ley de fibonnaci");
+a=0
+b=1
+c=a+b
+console.return (c);
---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+import { Inter } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+
+export default function HomePage() {
+  return (
+    <div className={styles.container}>
+
+      <head>
+        <title>Home - practica</title>
+        <meta name="description" content="Home Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Ir a <a href="/about">About</a>
+        </h1>
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
+        </p>)
+      </main>
+    </div>
+  )
+}
